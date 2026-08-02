72e3b57e25f7307d6b2d094ec1197ccf9b1c33f6
m2k3d <kulaginmatvei@gmail.com>
2026-08-02 16:25:05 +0700

    added width to neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,10 @@
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      window = {
+        width = 24,
+      },
+    },
   }
 }
